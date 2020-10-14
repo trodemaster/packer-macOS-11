b437d51888bf53d934f33a49d996450dd05f4e11
Blake Garner <blake@netjibbing.com>
2020-10-14 12:14:16 -0700

    tweak to get beta 10 to show up

--- scripts/softwareupdate.sh
+++ scripts/softwareupdate.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 shopt -s nullglob nocaseglob
 
+sudo /System/Library/PrivateFrameworks/Seeding.framework/Versions/A/Resources/seedutil enroll DeveloperSeed
 sudo softwareupdate -iaR
 
 exit 0
