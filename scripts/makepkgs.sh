c30fc1e4fad5b878e1260c9edad7c8821da9a9c9
Albert Koch <akoch@yelp.com>
2020-10-15 16:34:58 -0700

    increment uid

--- scripts/makepkgs.sh
+++ scripts/makepkgs.sh
@@ -1,2 +1,2 @@
 ./submodules/pycreateuserpkg/createuserpkg -n packer -u 502 -f packer -a -A -d -V 3 -i com.netjibbing.packer -p packer ./http/packer.pkg
-./submodules/pycreateuserpkg/createuserpkg -n vagrant -u 502 -f vagrant -a -A -d -V 3 -i com.netjibbing.vagrant -p vagrant ./http/vagrant.pkg
+./submodules/pycreateuserpkg/createuserpkg -n vagrant -u 503 -f vagrant -a -A -d -V 3 -i com.netjibbing.vagrant -p vagrant ./http/vagrant.pkg
