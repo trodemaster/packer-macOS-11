e39b5ee3aa80e0d6d701cdb0a096850f5808b5f0
Blake Garner <blake@netjibbing.com>
2020-10-31 11:04:01 -0700

    add hack to get packer comunicators working on big sur

---
+++ scripts/fakelease.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# get the bridge interface used by internet sharing
+BRIDGE_IF=$(pgrep -lf rtadvd | grep -E -o "bridge[0-9]+")
+
+# set the mac address prefix used by fusion
+MAC_ADDR_PREFIX=0:c:29
+
+# look for an arp entry with the same mac prefix
+ARP_TABLE=$(arp -i $BRIDGE_IF -a | grep $MAC_ADDR_PREFIX) 
+
+# extract the MAC
+VM_MAC=$(grep -E -o "([0-9a-fA-F]{1,2}[:-]{0,1}){5}[0-9a-fA-F]{1,2}" <<<$ARP_TABLE)
+
+# extract the IP 
+VM_IP=$(grep -E -o "([0-9]+\.){3}[0-9]+" <<<$ARP_TABLE)
+
+# write the fake leaes to teh config file
+sudo tee /private/var/db/vmware/vmnet-dhcpd-vmnet8.leases >/dev/null <<FAKELEASE
+lease $VM_IP {
+        starts 2 2020/03/17 20:05:20;
+        ends 2 2029/03/17 20:35:20;
+        hardware ethernet $VM_MAC;
+        uid 01:00:0c:29:0b:09:93;
+        client-hostname "faker";
+}
+FAKELEASE
