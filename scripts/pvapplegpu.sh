707d462755e132de4445fa7583d79bbb44728ac5
Blake Garner <blake@netjibbing.com>
2020-11-03 16:45:27 -0800

    add applegpu script

--- scripts/pvapplegpu.sh
+++ scripts/pvapplegpu.sh
@@ -8,31 +8,31 @@ else
 fi
 
 # svga.present
-if (grep -q svga.present $1); then
-  /opt/local/libexec/gnubin/sed -i 's/svga.present.*/svga.present="FALSE"/g' $1
+if (grep -q svga.present "$1"); then
+  /opt/local/libexec/gnubin/sed -i 's/svga.present.*/svga.present="FALSE"/g' "$1"
 else
-  echo 'svga.present="FALSE"' >>$1
+  echo 'svga.present="FALSE"' >>"$1"
 fi
 
 # appleGPU0.present
-if (grep -q appleGPU0.present $1); then
-  /opt/local/libexec/gnubin/sed -i 's/appleGPU0.present.*/appleGPU0.present="TRUE"/g' $1
+if (grep -q appleGPU0.present "$1"); then
+  /opt/local/libexec/gnubin/sed -i 's/appleGPU0.present.*/appleGPU0.present="TRUE"/g' "$1"
 else
-  echo 'appleGPU0.present="TRUE"' >>$1
+  echo 'appleGPU0.present="TRUE"' >>"$1"
 fi
 
 # appleGPU0.screenWidth
-if (grep -q appleGPU0.screenWidth $1); then
-  /opt/local/libexec/gnubin/sed -i 's/appleGPU0.screenWidth.*/appleGPU0.screenWidth=1920/g' $1
+if (grep -q appleGPU0.screenWidth "$1"); then
+  /opt/local/libexec/gnubin/sed -i 's/appleGPU0.screenWidth.*/appleGPU0.screenWidth=1920/g' "$1"
 else
-  echo 'appleGPU0.screenWidth=1920' >>$1
+  echo 'appleGPU0.screenWidth=1920' >>"$1"
 fi
 
 # appleGPU0.screenHeight
-if (grep -q appleGPU0.screenHeight $1); then
-  /opt/local/libexec/gnubin/sed -i 's/appleGPU0.screenHeight.*/appleGPU0.screenHeight1080/g' $1
+if (grep -q appleGPU0.screenHeight "$1"); then
+  /opt/local/libexec/gnubin/sed -i 's/appleGPU0.screenHeight.*/appleGPU0.screenHeight1080/g' "$1"
 else
-  echo 'appleGPU0.screenHeight=1080' >>$1
+  echo 'appleGPU0.screenHeight=1080' >>"$1"
 fi
 
 # view the config
