5030fe24165ce5e26b417550250b4e2e8419b872
Blake Garner <blake@netjibbing.com>
2020-11-05 12:49:55 -0800

    check for multiple vms running

--- scripts/fakelease.sh
+++ scripts/fakelease.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 # test for gnu sed
-if ! ( command -v /opt/local/libexec/gnubin/sed > /dev/null 2>&1 ); then
+if ! (command -v /opt/local/libexec/gnubin/sed >/dev/null 2>&1); then
   echo "This script requires gnu sed to work properly."
+  exit 1
+fi
+
+# check for only one running VM
+if [[ $(vmrun list | grep "Total running VMs" | cut -d ':' -f 2) -ne "1" ]]; then
+  echo "this script only works if one VM is running at a time"
+  vmrun list
   exit 1
 fi
 
@@ -13,13 +20,13 @@ BRIDGE_IF=$(pgrep -lf rtadvd | grep -E -o "bridge[0-9]+")
 MAC_ADDR_PREFIX=0:c:29
 
 # look for an arp entry with the same mac prefix
-ARP_TABLE=$(arp -i $BRIDGE_IF -a | grep $MAC_ADDR_PREFIX) 
+ARP_TABLE=$(arp -i $BRIDGE_IF -a | grep $MAC_ADDR_PREFIX)
 
 # extract the MAC
 VM_MAC=$(grep -E -o "([0-9a-fA-F]{1,2}[:-]{0,1}){5}[0-9a-fA-F]{1,2}" <<<$ARP_TABLE)
 VM_MAC_PADDED=$(/opt/local/libexec/gnubin/sed 's/\b\(\w\)\b/0\1/g' <<<$VM_MAC)
 
-# extract the IP 
+# extract the IP
 VM_IP=$(grep -E -o "([0-9]+\.){3}[0-9]+" <<<$ARP_TABLE)
 
 # write the fake leaes to teh config file
