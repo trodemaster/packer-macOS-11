bbf535827e2904ab3dff9f7091902110eaeca503
Blake Garner <blake@netjibbing.com>
2020-11-13 09:44:27 -0800

    use builtin sed so pvapplegpu is easier to use

--- scripts/pvapplegpu.sh
+++ scripts/pvapplegpu.sh
@@ -12,28 +12,28 @@ echo $VMX_FILE
 
 # svga.present
 if (grep -q svga.present $VMX_FILE); then
-  /opt/local/libexec/gnubin/sed -i 's/svga.present.*/svga.present="FALSE"/g' "$VMX_FILE"
+  /usr/bin/sed -i '' 's/svga.present.*/svga.present="FALSE"/g' "$VMX_FILE"
 else
   echo 'svga.present="FALSE"' >>"$VMX_FILE"
 fi
 
 # appleGPU0.present
 if (grep -q appleGPU0.present "$VMX_FILE"); then
-  /opt/local/libexec/gnubin/sed -i 's/appleGPU0.present.*/appleGPU0.present="TRUE"/g' "$VMX_FILE"
+  /usr/bin/sed -i '' 's/appleGPU0.present.*/appleGPU0.present="TRUE"/g' "$VMX_FILE"
 else
   echo 'appleGPU0.present="TRUE"' >>"$VMX_FILE"
 fi
 
 # appleGPU0.screenWidth
 if (grep -q appleGPU0.screenWidth "$VMX_FILE"); then
-  /opt/local/libexec/gnubin/sed -i 's/appleGPU0.screenWidth.*/appleGPU0.screenWidth=1920/g' "$VMX_FILE"
+  /usr/bin/sed -i '' 's/appleGPU0.screenWidth.*/appleGPU0.screenWidth=1920/g' "$VMX_FILE"
 else
   echo 'appleGPU0.screenWidth=1920' >>"$VMX_FILE"
 fi
 
 # appleGPU0.screenHeight
 if (grep -q appleGPU0.screenHeight "$VMX_FILE"); then
-  /opt/local/libexec/gnubin/sed -i 's/appleGPU0.screenHeight.*/appleGPU0.screenHeight1080/g' "$VMX_FILE"
+  /usr/bin/sed -i '' 's/appleGPU0.screenHeight.*/appleGPU0.screenHeight1080/g' "$VMX_FILE"
 else
   echo 'appleGPU0.screenHeight=1080' >>"$VMX_FILE"
 fi
