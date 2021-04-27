c45145f7f1887ba622674ce679346812e4f48757
Blake Garner <blake@netjibbing.com>
2021-04-27 09:27:41 -0700

    fixed 10.3 full install issue

--- scripts/os_settings.sh
+++ scripts/os_settings.sh
@@ -10,9 +10,11 @@ defaults -currentHost write com.apple.screensaver idleTime 0
 pkill Feedback || true
 
 # cleanup ssh enablement from install process
-sudo launchctl unload -w /Library/LaunchDaemons/ssh.plist
-sudo rm /Library/LaunchDaemons/ssh.plist
-#sudo /usr/sbin/systemsetup -f -setremotelogin on 
-sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist
+if [[ -e /Library/LaunchDaemons/ssh.plist ]]; then
+  sudo launchctl unload -w /Library/LaunchDaemons/ssh.plist
+  sudo rm /Library/LaunchDaemons/ssh.plist
+  #sudo /usr/sbin/systemsetup -f -setremotelogin on
+  sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist
+fi
 
 exit 0
