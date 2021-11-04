5b37d2d65729f86d952ff6154f16cdf8e01118ea
Blake Garner <blake@netjibbing.com>
2021-11-03 21:28:11 -0700

    remove host OS test

--- scripts/vmx_cleanup.sh
+++ scripts/vmx_cleanup.sh
@@ -14,21 +14,17 @@ else
   exit 0
 fi
 
-# check if host system is macOS 11.x or greater. If so enable pvgpu
-if grep -q -e '^11' <<<$(sw_vers -productVersion); then
-  echo "Adding pvgpu settings"
-  # svga.present
-  if (grep -q svga.present "$VMX_FILE"); then
-    /usr/bin/sed -i '' 's/svga.present.*/svga.present="FALSE"/g' "$VMX_FILE"
-  else
-    echo 'svga.present="FALSE"' >>"$VMX_FILE"
-  fi
+echo "Adding pvgpu settings"
+# svga.present
+if (grep -q svga.present "$VMX_FILE"); then
+  /usr/bin/sed -i '' 's/svga.present.*/svga.present="FALSE"/g' "$VMX_FILE"
+else
+  echo 'svga.present="FALSE"' >>"$VMX_FILE"
+fi
 
-  # appleGPU0.present
-  if (grep -q appleGPU0.present "$VMX_FILE"); then
-    /usr/bin/sed -i '' 's/appleGPU0.present.*/appleGPU0.present="TRUE"/g' "$VMX_FILE"
-  else
-    echo 'appleGPU0.present="TRUE"' >>"$VMX_FILE"
-  fi
-
+# appleGPU0.present
+if (grep -q appleGPU0.present "$VMX_FILE"); then
+  /usr/bin/sed -i '' 's/appleGPU0.present.*/appleGPU0.present="TRUE"/g' "$VMX_FILE"
+else
+  echo 'appleGPU0.present="TRUE"' >>"$VMX_FILE"
 fi
