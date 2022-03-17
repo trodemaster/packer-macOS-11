aaa81bd8da4a416f694af6f1c4a5579672d6d5cf
Blake Garner <blake@netjibbing.com>
2022-03-16 21:40:12 -0700

    testing newuser

--- scripts/newuser.sh
+++ scripts/newuser.sh
@@ -8,19 +8,25 @@ SCRIPT=$(realpath $0)
 SCRIPTPATH=$(dirname $SCRIPT)
 
 # new user name var
+NEW_USERNAME="vagrant"
 # new user pass var
+NEW_PASS="vagrant"
 
-. /etc/rc.common
-dscl . create /Users/administrator
-dscl . create /Users/administrator RealName "Terminal User Account"
-dscl . create /Users/administrator hint "Password Hint"
-dscl . create /Users/administrator picture "/Path/To/Picture.png"
-dscl . passwd /Users/administrator thisistheaccountpassword
-dscl . create /Users/administrator UniqueID 501
-dscl . create /Users/administrator PrimaryGroupID 20
-dscl . create /Users/administrator UserShell /bin/zsh
-dscl . create /Users/administrator NFSHomeDirectory /Users/administrator
-cp -R /System/Library/User\ Template/English.lproj /Users/administrator
-chown -R administrator:staff /Users/administrator
+
+
+sysadminctl -addUser "$NEW_USERNAME" -fullName "Administrator" -password Password -home /Users/"$NEW_USERNAME" -admin -shell /bin/zsh -picture /Users/Shared/Adminicon.jpg
+
+# . /etc/rc.common
+# dscl . create /Users/administrator
+# dscl . create /Users/administrator RealName "Terminal User Account"
+# dscl . create /Users/administrator hint "Password Hint"
+# dscl . create /Users/administrator picture "/Path/To/Picture.png"
+# dscl . passwd /Users/administrator thisistheaccountpassword
+# dscl . create /Users/administrator UniqueID 501
+# dscl . create /Users/administrator PrimaryGroupID 20
+# dscl . create /Users/administrator UserShell /bin/zsh
+# dscl . create /Users/administrator NFSHomeDirectory /Users/administrator
+# cp -R /System/Library/User\ Template/English.lproj /Users/administrator
+# chown -R administrator:staff /Users/administrator
 
 # startup launchd to remove packer account and itself
